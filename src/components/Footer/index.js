6b6bc279c2992c97ff66990c13cbc437faa06960
emanoell <emanoel.lopes.web@gmail.com>
2019-05-17 14:07:30 -0300

    fix lighthouse acessbility audit and update snapshot

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer () {
     <footer className="rm-footer">
       <div className="container">
         <span>
-          <a href="https://emanoellopes.me" rel="noopener noreferrer" target="_blank">Emanoel Lopes</a> <a href="https://github.com/emanoellopes" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a> &bull; Copyright &copy; 2019 &bull; Made with <a className="react-site-link" aria-label="Check more in React.js Official Site" href="https://reactjs.org/" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faReact} /></a> and <FontAwesomeIcon icon={faHeart} />
+          <a href="https://emanoellopes.me" rel="noopener noreferrer" aria-label="Link to Emanoel Lopes Personal Website" target="_blank">Emanoel Lopes</a> <a href="https://github.com/emanoellopes" aria-label="Link to Emanoel Lopes Github" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faGithub} /></a> &bull; Copyright &copy; 2019 &bull; Made with <a className="react-site-link" aria-label="Check more in React.js Official Site" href="https://reactjs.org/" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faReact} /></a> and <FontAwesomeIcon icon={faHeart} />
         </span>
       </div>
     </footer>
